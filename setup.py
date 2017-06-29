e69d9edb1ad317bebf246229bded49a71dac7760
Martin Valgur <martin.valgur@gmail.com>
2017-06-29 18:43:49 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-socket',
-    version='0.1.0',
+    version='0.2.0b0',
     author='Mike Fiedler',
     author_email='miketheman@gmail.com',
     maintainer='Mike Fiedler',
